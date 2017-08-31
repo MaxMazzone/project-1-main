a96327f51e319c1148a2cd788b84ddd0ac5df28c
stgeorge3 <amaxmazzone@gmail.com>
2017-08-31 16:49:58 -0400

    Add functions and UI events for sign in, up, out, and change password added event listeners to buttons on forms. Create functions to get form information and conduct ajax requests.

--- assets/scripts/auth/events.js
+++ assets/scripts/auth/events.js
@@ -9,42 +9,39 @@ const onSignUp = function (event) {
   const data = getFormFields(this)
   console.log(data)
   event.preventDefault()
-  // api.signUp(data)
-  //   .then(ui.signUpSuccess)
-  //   .catch(ui.signUpFailure)
+  api.signUp(data)
+    .then(ui.signUpSuccess)
+    .catch(ui.signUpFailure)
 }
-// const onSignIn = function (event) {
-//   const data = getFormFields(this)
-//   console.log(data)
-//   event.preventDefault()
-//   api.signIn(data)
-//     .then(ui.signInSuccess)
-//     .catch(ui.signInFailure)
-// }
-// const onChangePassword = function (event) {
-//   const data = getFormFields(this)
-//   console.log(data)
-//   event.preventDefault()
-//   api.changePassword(data)
-//     .then(ui.changePasswordSuccess)
-//     .catch(ui.changePasswordFailure)
-// }
-// const onSignOut = function (event) {
-//   console.log('on signout in events.js')
-//   event.preventDefault()
-//   api.signOut()
-//     .then(ui.signOutSuccess)
-//     .catch(ui.signOutFailure)
-// }
+const onSignIn = function (event) {
+  const data = getFormFields(this)
+  console.log(data)
+  event.preventDefault()
+  api.signIn(data)
+    .then(ui.signInSuccess)
+    .catch(ui.signInFailure)
+}
+const onChangePassword = function (event) {
+  const data = getFormFields(this)
+  console.log(data)
+  event.preventDefault()
+  api.changePassword(data)
+    .then(ui.changePasswordSuccess)
+    .catch(ui.changePasswordFailure)
+}
+const onSignOut = function (event) {
+  console.log('on signout in events.js')
+  event.preventDefault()
+  api.signOut()
+    .then(ui.signOutSuccess)
+    .catch(ui.signOutFailure)
+}
 
 const addHandlers = function () {
-  $('#sign-up').on('submit', function (event) {
-    event.preventDefault()
-    console.log('sign-up event handler working')
-  })
-  // $('#sign-in').on('submit', onSignIn)
-  // $('#change-password').on('submit', onChangePassword)
-  // $('#sign-out').on('submit', onSignOut)
+  $('#sign-up').on('submit', onSignUp)
+  $('#sign-in').on('submit', onSignIn)
+  $('#change-password').on('submit', onChangePassword)
+  $('#sign-out').on('submit', onSignOut)
 }
 
 module.exports = {
