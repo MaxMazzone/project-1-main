5d5596f1cb21260bdec514f5eb1d9cb25d94644b
stgeorge3 <amaxmazzone@gmail.com>
2017-08-31 12:26:26 -0400

    Add checkforwinner function Add function to check each row for winning combination and announce winner if match is found.

--- assets/scripts/events.js
+++ assets/scripts/events.js
@@ -1,8 +1,4 @@
 const gameLogic = require('./gamelogic.js')
-const putGamePiece = function () {
-  $('#1').text(gameLogic.userTurn)
-  console.log(gameLogic.userTurn)
-}
 
 $(() => {
   $('.container').find('.coord').on('click', gameLogic.runGame)
--- assets/scripts/gamelogic.js
+++ assets/scripts/gamelogic.js
@@ -6,6 +6,9 @@ const addTokenToArray = function (userClick, userTurn) {
   gameArray[userClick - 1] = userTurn
   return gameArray
 }
+const putGamePiece = function (id) {
+  $('#' + id).text(userTurn)
+}
 
 const changeTurn = function () {
   if (userTurn === 'x') {
@@ -15,15 +18,43 @@ const changeTurn = function () {
   }
   return userTurn
 }
+const checkForWinner = function (userTurn) {
+  let result = false
+  if (checkRow(1, 2, 3, userTurn) ||
+ checkRow(4, 5, 6, userTurn) ||
+ checkRow(7, 8, 9, userTurn) ||
+ checkRow(1, 4, 7, userTurn) ||
+ checkRow(2, 5, 8, userTurn) ||
+ checkRow(3, 6, 9, userTurn) ||
+ checkRow(1, 5, 9, userTurn) ||
+ checkRow(3, 5, 7, userTurn)) {
+    result = true
+    console.log('there is a winner' + result)
+  }
+  return result
+}
+
+const checkRow = function checkRow (a, b, c, userTurn) {
+  let result = false
+  if (gameArray[a - 1] === userTurn && gameArray[b - 1] === userTurn && gameArray[c - 1] === userTurn) {
+    result = true
+  }
+  return result
+}
 
 const runGame = function () {
+  const id = this.id
   if (gameArray[this.id - 1] === null) {
     if (userTurn === 'x') {
+      putGamePiece(id)
       addTokenToArray(this.id, userTurn)
+      checkForWinner(userTurn)
       changeTurn()
       console.log(gameArray)
     } else {
+      putGamePiece(id)
       addTokenToArray(this.id, userTurn)
+      checkForWinner(userTurn)
       changeTurn()
       console.log(gameArray)
     }
