bc027487d7e13b425efda557e2ec053718c06371
stgeorge3 <amaxmazzone@gmail.com>
2017-08-31 08:45:21 -0400

    Add function to add token to the game array created function which runs during game logic to add the user token to the game array

--- assets/scripts/gamelogic.js
+++ assets/scripts/gamelogic.js
@@ -1,23 +1,32 @@
 const gameArray = [null, null, null, null, null, null, null, null, null]
-let userClick = 2
-let userTurn = 'x'
 
-if (userClick === 1 && gameArray[userClick - 1] === null) {
-  if (userTurn === 'x') {
-    console.log('x is in topleft')
-    userTurn = 'o'
-  } else {
-    console.log('o is on topleft')
-    userTurn = 'x'
+const addTokenToArray = function (userClick, userTurn) {
+  gameArray[userClick - 1] = userTurn
+  return gameArray
+}
+
+const runGame = function (userClick, userTurn) {
+  if (userClick === 1 && gameArray[userClick - 1] === null) {
+    if (userTurn === 'x') {
+      addTokenToArray(userClick, userTurn)
+      console.log('x is in topleft')
+      userTurn = 'o'
+    } else {
+      addTokenToArray(userClick, userTurn)
+      console.log('o is on topleft')
+      userTurn = 'x'
+    }
+  } else if (userClick === 2 && gameArray[userClick - 1] === null) {
+    if (userTurn === 'x') {
+      addTokenToArray(userClick, userTurn)
+      console.log('x is in middle left')
+      userTurn = 'o'
+    } else {
+      addTokenToArray(userClick, userTurn)
+      console.log('o is on middle left')
+      userTurn = 'x'
+    }
+  } else if (gameArray[userClick - 1] !== null) {
+    console.log('invalid move')
   }
-} else if (userClick === 2 && gameArray[userClick - 1] === null) {
-  if (userTurn === 'x') {
-    console.log('x is in middle left')
-    userTurn = 'o'
-  } else {
-    console.log('o is on middle left')
-    userTurn = 'x'
-  }
-} else if (gameArray[userClick - 1] !== null) {
-  console.log('invalid move')
 }
